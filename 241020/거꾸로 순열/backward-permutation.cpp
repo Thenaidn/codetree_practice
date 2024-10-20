ae4a981cb9349ee81cb87d57ebcb6701f9c283e8
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-10-20 21:31:22 +0900

    Title: 거꾸로 순열, Time: 86ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/거꾸로 순열/backward-permutation.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <unordered_map>
+#include <climits>
+using namespace std;
+
+int a, b, c, d, e;
+int dy[4] = { 0, -1, 0, 1 };
+int dx[4] = { -1, 0, 1, 0 };
+
+bool visited[1000] = { false };
+
+int n = 0;
+vector<int> answer;
+int res = INT_MAX;
+char maps[20][20];
+int tmpmaps[20][20] = { 0, };
+vector<pair<int, int>> points;
+
+void Print(vector<int> ans) {
+    for (int a : ans) {
+        cout << a + 1 << " ";
+    }
+    cout << endl;
+}
+int calc(vector<int>& ans) {
+    int max_dist = 0;
+    for (int i = 0; i < b; i++) {
+        for (int j = i + 1; j < b; j++) {
+            int dx = points[ans[i]].first - points[ans[j]].first;
+            int dy = points[ans[i]].second - points[ans[j]].second;
+            max_dist = max(max_dist, dx * dx + dy * dy);
+        }
+    }
+    return max_dist;
+}
+void Choose(int curr_num) {
+    if (curr_num == a) {
+
+        Print(answer);
+        return;
+    }
+
+    for (int i = a - 1; i >= 0; i--) {
+        if (!visited[i]) {
+            answer.push_back(i);
+            visited[i] = true;
+        }
+        else { continue; }
+        Choose(curr_num + 1);
+
+        answer.pop_back();
+        visited[i] = false;
+    }
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> a;
+
+
+    Choose(0);
+    
+    return 0;
+}

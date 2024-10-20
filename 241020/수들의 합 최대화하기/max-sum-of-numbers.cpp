3fed27eaac59427c6ee47c6e044dd2f9342e2202
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-10-20 21:50:53 +0900

    Title: 수들의 합 최대화하기, Time: 32ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/수들의 합 최대화하기/max-sum-of-numbers.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <unordered_map>
+#include <climits>
+using namespace std;
+
+int a, b, c, d, e;
+int dy[4] = { 0, -1, 0, 1 };
+int dx[4] = { -1, 0, 1, 0 };
+
+bool visited[1000] = { false };
+
+int n = 0;
+vector<int> answer;
+int res = INT_MIN;
+int maps[20][20];
+int tmpmaps[20][20] = { 0, };
+vector<pair<int, int>> points;
+
+void Print(vector<int> ans) {
+    for (int a : ans) {
+        cout << a << " ";
+    }
+    cout << endl;
+}
+int calc(vector<int>& ans) {
+    int sum = 0;
+    for (int i = 0; i < a; i++) {
+        sum += maps[i][answer[i]];
+    }
+    return sum;
+}
+void Choose(int curr_num) {
+    if (curr_num == a) {
+        res = max(res, calc(answer));
+        //Print(answer);
+        return;
+    }
+
+    for (int i = 0; i < a; i++) {
+        if (!visited[i]) {
+            visited[i] = true;
+            answer.push_back(i);
+            Choose(curr_num + 1); 
+            answer.pop_back(); 
+            visited[i] = false;
+        }
+    }
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> a;
+
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < a; j++) {
+            cin >> maps[i][j];
+        }
+    }
+
+
+    Choose(0);
+    cout << res;
+    return 0;
+}

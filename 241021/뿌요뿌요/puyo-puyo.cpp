4385a519451183553a2969910f94629dfd0bd5ec
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-10-21 02:19:09 +0900

    Title: 뿌요뿌요, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241021/뿌요뿌요/puyo-puyo.cpp
+++ 241021/뿌요뿌요/puyo-puyo.cpp
@@ -22,20 +22,18 @@ bool isinside(int y, int x) {
     return y >= 0 && y < a && x >= 0 && x < b;
 }
 
-int cnt = 0;
-void dfs(int y, int x, int k) {
 
+int dfs(int y, int x, int k) {
+    visited[y][x] = true;
+    int cnt = 1;
     for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
         int ny = y + dy[i];
-        if (isinside(ny, nx)) {
-            if (maps[ny][nx] == k && visited[ny][nx] == false) {
-                visited[ny][nx] = true;
-                cnt++;
-                dfs(ny, nx, k);
-            }
+        int nx = x + dx[i];
+        if (isinside(ny, nx) && maps[ny][nx] == k && !visited[ny][nx]) {
+            cnt += dfs(ny, nx, k);
         }
     }
+    return cnt;
 }
 
 int main() {
@@ -44,34 +42,29 @@ int main() {
 
     cin >> a;
     b = a;
-    int maxnum = 0;
     for (int i = 0; i < a; i++) {
         for (int j = 0; j < b; j++) {
             cin >> maps[i][j];
-            maxnum = max(maxnum, maps[i][j]);
         }
     }
-    for (int k = 1; k <= maxnum; k++) {
-        bool completed = false;
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-                if (!visited[i][j] && maps[i][j] == k) {
-                    dfs(i, j, k);
-                    if (cnt > 3) { 
-                        if(completed){ res++; }
-                        completed = true;
-                    }
-                    answer.push_back(cnt);
-                    cnt = 0;
+
+    int res = 0;
+    int ans = 0;
+
+
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < b; j++) {
+            if (!visited[i][j]) {
+                int size = dfs(i, j, maps[i][j]);
+                if (size >= 4) {
+                    res++;
+                    ans = max(ans, size);
                 }
             }
         }
+    }
 
-    }
-    int ans = 0;
-    for (int k = 0; k< answer.size(); k++) {
-        ans = max(ans, answer[k]);
-    }
+
     
     cout << res << " " << ans;
 

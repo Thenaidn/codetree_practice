f9262abd58398c31724329a8cd87eac5e71b0de1
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-10-27 19:43:49 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 241027/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
+++ 241027/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -15,23 +15,43 @@ int a, b, c, d, e, k, n;
 int dy[4] = { 0, 1, 0, -1 };
 int dx[4] = { 1, 0, -1, 0 };
 
-int memo[MAX_N] = { -1 };
+long long memo[MAX_N] = { -1 };
 int dp[MAX_N][MAX_N] = { 0, };
-int dp2[MAX_N][MAX_N] = { 0, };
 int maps[MAX_N][MAX_N];
 
-
+int res = INT_MAX;
 
 void initialize() {
-    dp[0][0] = maps[0][0];
-    dp2[0][0] = maps[0][0];
-    for (int i = 1; i < a; i++) {
-        dp[0][i] = min(dp[0][i - 1], maps[0][i]);
-        dp[i][0] = min(dp[i - 1][0], maps[i][0]);
+    
+}
 
-        dp2[0][i] = max(dp2[0][i - 1], maps[0][i]);
-        dp2[i][0] = max(dp2[i - 1][0], maps[i][0]);
+int calc(int low) {
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < a; j++) {
+            dp[i][j] = 1001;
+        }
     }
+
+    if (maps[0][0] < low) { return 1001; }
+    dp[0][0] = maps[0][0];
+
+    for (int i = 1; i < a; i++) {
+        if (maps[i][0] >= low) {
+            dp[i][0] = max(dp[i - 1][0], maps[i][0]);
+        }
+        if (maps[0][i] >= low) {
+            dp[0][i] = max(dp[0][i - 1], maps[0][i]);
+        }
+    }
+
+    for (int i = 1; i < a; i++) {
+        for (int j = 1; j < a; j++) {
+            if (maps[i][j] >= low) {
+                dp[i][j] = max(min(dp[i - 1][j], dp[i][j - 1]), maps[i][j]);
+            }
+        }
+    }
+    return dp[a - 1][a - 1];
 }
 
 int main() {
@@ -45,32 +65,19 @@ int main() {
         }
     }
 
-    if (a == 1) { cout << 0; return 0; }
+    if (a == 1) { cout << maps[0][0]; return 0; }
 
     initialize();
 
-    for (int i = 1; i < a; i++) {
-        for (int j = 1; j < a; j++) {
-            int max_up = max(dp2[i - 1][j], maps[i][j]);
-            int min_up = min(dp[i - 1][j], maps[i][j]);
-            int max_left = max(dp2[i][j - 1], maps[i][j]);
-            int min_left = min(dp[i][j - 1], maps[i][j]);
+    
+    for (int low = 1; low <= 100; low++) {
+        int m = calc(low);
 
-            if (abs(max_up - min_up) < abs(max_left - min_left)) {
-                dp2[i][j] = max_up;
-                dp[i][j] = min_up;
-            }
-            else if (abs(max_up - min_up) == abs(max_left - min_left)) {
-                dp2[i][j] = min(max_up, max_left);
-                dp[i][j] = max(min_up, min_left);
-            }
-            else {
-                dp2[i][j] = max_left;
-                dp[i][j] = min_left;
-            }
+        if (m != 1001) {
+            res = min(res, m - low);
         }
     }
 
-    cout << abs(dp[a - 1][a - 1] - dp2[a-1][a-1]);
+    cout << res;
     return 0;
 }

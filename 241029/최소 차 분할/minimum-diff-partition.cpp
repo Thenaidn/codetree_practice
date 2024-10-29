79b8e7f4db6589b11a3c16728efbdb6a3541a7e2
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-10-29 12:22:27 +0900

    Title: 최소 차 분할, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241029/최소 차 분할/minimum-diff-partition.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <unordered_map>
+#include <climits>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+#define MAX_N 100005
+#define MOD 10007
+
+int a, b, c, d, e, k, n;
+int dy[4] = { 0, 1, 0, -1 };
+int dx[4] = { 1, 0, -1, 0 };
+
+long long memo[MAX_N] = { -1 };
+int dp[MAX_N] = { 0, };
+vector<int> arr;
+
+bool ans[MAX_N] = { false, };
+
+void initialize() {
+    for (int i = 0; i <= MAX_N; i++) {
+        dp[i] = INT_MAX;
+    }
+    dp[0] = 0;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int sum = 0;
+   
+    cin >> b;
+    for (int i = 0; i < b; i++) {
+        cin >> c;
+        arr.push_back(c);
+        sum += c;
+
+    }
+
+    initialize();
+
+
+
+    for (int num : arr) {
+        for (int i = MAX_N; i >= num; i--) {
+            if (dp[i - num] != INT_MAX) {
+                dp[i] = min(dp[i], dp[i - num] + 1);
+                ans[abs(i + i - sum)] = true;
+                
+            }
+        }
+    }
+    for (int i = 0; i <= MAX_N; i++) {
+        if (ans[i]) {
+            cout << i;
+            break;
+        }
+    }
+    return 0;
+}

aa2f24ce016c6377075b9dbc2a8619e152ae2692
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-10-31 22:31:43 +0900

    Title: 신전 탐험하기 3, Time: 13ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241031/신전 탐험하기 3/explore-temple-3.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+#define MAX_N 1001
+#define MOD 1000000007
+
+int n, m;
+int treasure[MAX_N][3];
+int dp[MAX_N][101];
+
+void initialize() {
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < m; j++) {
+            dp[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        dp[1][i] = treasure[0][i];
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> treasure[i][j];
+        }
+    }
+
+    initialize();
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < m; k++) {
+                if (j != k) {
+                    int tmp = dp[i - 1][k] + treasure[i - 1][j];
+                    dp[i][j] = max(dp[i][j], tmp);
+                }
+            }
+        }
+    }
+    int res = 0;
+    for (int i = 0; i < m; i++) {
+        res = max(res, dp[n][i]);
+    }
+
+    cout << res;
+    return 0;
+}

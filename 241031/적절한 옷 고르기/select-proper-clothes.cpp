d71c4e7d3eb6125962785e3469f5f6c7a7c95cc3
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-10-31 20:12:58 +0900

    Title: 적절한 옷 고르기, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 241031/적절한 옷 고르기/select-proper-clothes.cpp
+++ 241031/적절한 옷 고르기/select-proper-clothes.cpp
@@ -41,7 +41,7 @@ int main() {
         for (int j = 0; j < n; j++) {
             if (i < shirts[j].first.first || i > shirts[j].first.second) { continue; }
             for (int k = 0; k < n; k++) {
-                if (dp[i - 1][k] != -1 && j != k) {
+                if (dp[i - 1][k] != -1) {
                     dp[i][j] = max(dp[i][j], dp[i - 1][k] +
                         abs(shirts[k].second - shirts[j].second));
                 }

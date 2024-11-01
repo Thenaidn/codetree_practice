b7844c3b8d497a42819f2d90677f88c9d9f51ce5
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-01 20:58:17 +0900

    Title: 대응되는 수와 문자, Time: 21ms, Memory: 1MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241101/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+#define MAX_N 10001
+#define MOD 1000000007
+
+int n, m;
+int a, b, c, d, e;
+
+string arr[MAX_N];
+
+string t;
+
+int main() {
+    unordered_map<string, int> umap; // 정수를 관리할 hashmap을 선언합니다. => 빈 hashmap
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        umap[arr[i]] = i + 1;
+    }
+
+    int res = 0;
+
+    for (int i = 0; i < m; i++) {
+        string tmp;
+        cin >> tmp;
+        try {
+            int num = stoi(tmp);
+            cout << arr[num - 1] << endl;
+        }
+        catch (const std::invalid_argument& e) {
+            cout << umap[tmp] << endl;
+        }
+    }
+
+    //cout << res;
+
+    return 0;
+
+
+}

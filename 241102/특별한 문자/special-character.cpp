e8bd032159df55a4360e50249cd63a02f1939962
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-02 16:39:54 +0900

    Title: 특별한 문자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241102/특별한 문자/special-character.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+#define MAX_N 5001
+#define MOD 1000000007
+
+int n, m;
+int a, b, c, d, e;
+
+int arr[4][MAX_N];
+
+string t;
+
+bool cmp(pair<char, int> a, pair<char, int>b) {
+    return a.first > b.first;
+}
+
+int main() {
+    unordered_map<char, int> umap; // 정수를 관리할 hashmap을 선언합니다. => 빈 hashmap
+
+    cin >> t;
+
+    for (char i : t) {
+        if (umap.find(i) == umap.end()) {
+            umap[i] = 1;
+        }
+        else {
+            int tmp = umap[i];
+            umap.erase(i);
+            umap[i] = ++tmp;
+        }
+    }
+   
+    
+    int count = 0;
+
+    for (pair<char, int> p : umap) {
+        if (p.second == 1) {
+            cout << p.first;
+            return 0;
+        }
+    }
+    
+    //cout << count;
+
+    return 0;
+
+
+}

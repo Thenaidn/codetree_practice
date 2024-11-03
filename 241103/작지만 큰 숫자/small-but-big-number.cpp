37de63cf77d864ff2dde171c5120afe5e71883fd
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-03 21:41:24 +0900

    Title: 작지만 큰 숫자, Time: 102ms, Memory: 1MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241103/작지만 큰 숫자/small-but-big-number.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <string>
+#include <map>
+#include <unordered_set>
+#include <queue>
+#include <set>
+using namespace std;
+
+#define MAX_N 100001
+#define MOD 1000000007
+
+int n, m;
+int a, b, c, d, e;
+
+int arr[4][MAX_N];
+
+string t;
+
+
+
+int main() {
+
+    cin >> n >> m;
+    set<int> s;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        s.insert(a);
+    }
+
+    for (int i = 0; i < m; i++) {
+        cin >> b;
+        auto it = s.upper_bound(b);
+        it--;
+        if (it != s.end()) {
+            cout << *it << endl;
+            s.erase(*it);
+        }
+        else {
+            cout << -1 << endl;
+        }
+    }
+}

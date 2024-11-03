51ec23a97c8c723c23252008acdd3ccb1f38149d
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-03 20:07:05 +0900

    Title: top k 숫자, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241103/top k 숫자/top-k-elements.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+#include <string>
+#include <map>
+#include <unordered_set>
+#include <queue>
+#include <set>
+using namespace std;
+
+#define MAX_N 100001
+#define MOD 1000000007
+
+int n, m;
+int a, b, c, d, e;
+
+int arr[4][MAX_N];
+
+string t;
+
+
+
+int main() {
+    set<int> s;
+
+    s.insert(0);
+    cin >> n >> m;
+
+    int res;
+    for (int i = 0; i < n; i++) { 
+        cin >> b; s.insert(b); 
+    }
+    set<int>::iterator it;
+    it = s.upper_bound(b - 1);
+    for (int i = 0; i < m; i++) {
+        cout << *it << " ";
+        it--;
+    }
+
+
+    
+    return 0;
+}

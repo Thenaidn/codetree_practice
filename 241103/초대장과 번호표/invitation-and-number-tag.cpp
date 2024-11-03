b840604301a4d23cfb222f16206a450dfe49fb22
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-03 18:17:41 +0900

    Title: 초대장과 번호표, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 241103/초대장과 번호표/invitation-and-number-tag.cpp
+++ 241103/초대장과 번호표/invitation-and-number-tag.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <map>
 #include <unordered_set>
+#include <queue>
 using namespace std;
 
 #define MAX_N 100001
@@ -18,54 +19,46 @@ int arr[4][MAX_N];
 
 string t;
 
-vector<int> groups[250001];
+queue<int> q;
+unordered_set<int> set[250001];
+unordered_set<int> invited;
 
+void bfs() {
+    while (!q.empty()) {
+        int tmp = q.front();
+        q.pop();
+
+        for (int i = 0; i < m; i++) {
+            if (set[i].find(tmp) != set[i].end()) {
+                set[i].erase(tmp);
+
+                if (set[i].size() == 1) {
+                    invited.insert(*set[i].begin());
+                    q.push(*set[i].begin());
+                }
+            }
+        }
+    }
+}
 
 int main() {
     cin >> n >> m;
 
-    unordered_set<int> set;
     for (int i = 0; i < m; i++) {
         cin >> a;
         for (int j = 0; j < a; j++) {
             cin >> b;
-            if (j == 0) { set.insert(b); }
-            groups[i].push_back(b);
+            set[i].insert(b);
         }
     }
+    invited.insert(1);
+    q.push(1);
 
-    /*for (int i = m - 1; i >= 0; i--) {
-        int count = 1;
-        for (int j = 1; j < groups[i].size(); j++) {
-            if (set.find(groups[i][j]) != set.end()) {
-                count++;
-            }
-            
-        }
-        if (count == groups[i].size() - 1) {
-            for (int j = 1; j < groups[i].size(); j++) {
-                set.insert(groups[i][j]);
-            }
-        }
-    }*/
+    bfs();
+
     int res = 0;
 
-    for (int i = 0; i < m; i++) {
-        int count = 1;
-        for (int j = 1; j < groups[i].size(); j++) {
-            if (set.find(groups[i][j]) != set.end()) {
-                count++;
-            }
-
-        }
-        if (count == groups[i].size() - 1) {
-            for (int j = 1; j < groups[i].size(); j++) {
-                set.insert(groups[i][j]);
-            }
-        }
-    }
-
-    res = set.size();
+    res = invited.size();
     cout << res;
     return 0;
 

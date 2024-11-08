b44b62dc51cdd43bf023b5b85cd46e9548a639c4
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-08 18:36:03 +0900

    Title: 최대 직사각형 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241108/최대 직사각형 합/max-rect-sum-in-grid.cpp
+++ 241108/최대 직사각형 합/max-rect-sum-in-grid.cpp
@@ -2,41 +2,46 @@
 #include <climits>
 #include <algorithm>
 using namespace std;
-int a,b,c,d,e;
+
 int main() {
-    int arr[1001][1001];
-    int prefix_sum[1001][1001] = {};
-    cin >> a >> b;
+    int n;
+    cin >> n;
+    int arr[301][301] = {}; int prefix_sum[301][301] = {};
 
-    for(int i = 1; i <= a; i++)
-        for(int j = 1; j <= a; j++)
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
             cin >> arr[i][j];
+        }
+    }
 
-    for(int i = 1; i <= a; i++)
-        for(int j = 1; j <= a; j++)
-            prefix_sum[i][j] = prefix_sum[i - 1][j] + 
-                               prefix_sum[i][j - 1] -
-                               prefix_sum[i - 1][j - 1] +
-                               arr[i][j];
+    // prefix_sum 계산하기
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            prefix_sum[i][j] = arr[i][j] + prefix_sum[i - 1][j] + prefix_sum[i][j - 1] - prefix_sum[i - 1][j - 1];
+        }
+    }
 
-    int m = INT_MIN;
+    int max_sum = INT_MIN;
+    
+    for (int top = 1; top <= n; top++) {
+        for (int bottom = top; bottom <= n; bottom++) {
+            
+            int current_sum = 0;
+            for (int col = 1; col <= n; col++) {
+                int sum = prefix_sum[bottom][col] - prefix_sum[top - 1][col];
+                current_sum += sum;
 
-    for(int i = 1; i <= a; i++){
-        for(int j = 1; j <= a; j++){
-            for(int k=1; k<=a;k++){
-                if(i-k>=0&&j-k>=0){
-                    m = max(m, prefix_sum[i][j] - 
-                    prefix_sum[i][j-k] - prefix_sum[i-k][j] + prefix_sum[i-k][j-k]);
+                if (current_sum > max_sum) {
+                    max_sum = current_sum;
                 }
-                else if(j-k>=0){
-                    m = max(m, prefix_sum[i][j] - prefix_sum[i][j-k]);
-                }
-                else if(i-k>=0){
-                    m = max(m, prefix_sum[i][j] - prefix_sum[i-k][j]);
+                
+                if (current_sum < 0) {
+                    current_sum = 0;
                 }
             }
         }
     }
-    cout << m;
+
+    cout << max_sum << endl;
     return 0;
 }

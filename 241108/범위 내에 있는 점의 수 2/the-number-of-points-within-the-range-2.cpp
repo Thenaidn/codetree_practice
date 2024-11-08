c403f836a094a7f3d4080c4bd6eaf08dcea37eda
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-08 17:50:47 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 139ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

--- 241108/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.cpp
+++ 241108/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.cpp
@@ -4,32 +4,24 @@
 using namespace std;
 
 int main() {
-    int arr[100001];
+    int arr[100001] = { 0 };
     vector<int> v;
-    int n,q;
-    cin >> n>> q;
-    int p=0;
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
-        v.push_back(arr[i]);
-    }
-    sort(v.begin(), v.end());
+    int n, q;
+    cin >> n >> q;
+    int p = 0;
+    int afds;
 
-    for(int i=0;i<q;i++){
-        int a,b;
+    for (int i = 0; i < n; i++) {
+        cin >> afds;
+        for (int i = afds; i < 100001; i++) { arr[i]++; }
+        //v.push_back(arr[i]);
+    }
+    //sort(v.begin(), v.end());
+
+    for (int i = 0; i < q; i++) {
+        int a, b;
         cin >> a >> b;
-        int count1 = 0; int count2 = 0;
-        for(int j:v){
-            if(j < a){
-                count1++;
-            }
-        }
-        count2 = count1;
-        for(int j=count1;j<v.size();j++){
-            if(v[j] <= b){count2++;}
-            else{break;}
-        }
-        cout << count2 - count1 << endl;
+        cout << arr[b] - arr[a - 1] << endl;
     }
     // 여기에 코드를 작성해주세요.
     return 0;

c9963525b08ff9bc97d073c224381f62bba34403
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-09 02:49:25 +0900

    Title: 점 개수 세기, Time: 282ms, Memory: 1MB, Status: Passed - CodeTree

--- 241109/점 개수 세기/count-number-of-points.cpp
+++ 241109/점 개수 세기/count-number-of-points.cpp
@@ -1,62 +1,30 @@
 #include <iostream>
-#include <set>
-#include <unordered_map>
-#include <utility>
-#include <map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
     int n, q;
     cin >> n >> q;
-    int edges[100001];
-    set<int> nums;
+    
+    vector<int> points(n);
     for (int i = 0; i < n; i++) {
-        cin >> edges[i];
-        nums.insert(edges[i]);
-    }
-    map<int, int> mapper;
-    int cnt = 1;
-    for (int i : nums) {
-        mapper[i] = cnt;
-        cnt++;
+        cin >> points[i];
     }
 
+    sort(points.begin(), points.end());
+    
     for (int i = 0; i < q; i++) {
         int a, b;
         cin >> a >> b;
-        int x1, x2;
-        auto x11 = mapper.lower_bound(a);
-
-
-        auto x22 = mapper.lower_bound(b + 1);
-
-        if (x22 == mapper.end()) {
-            x22--;
-            if (x22->first > b) {
-                x22 = mapper.end();
-            }
-        }
-        else {
-            x22--;
-        }
-
-        if (x11 != mapper.end() && x22 != mapper.end()) {
-            x1 = x11->second; x2 = x22->second;
-
-            cout << x2 - x1 + 1 << endl;
-        }
-
         
+        auto left = lower_bound(points.begin(), points.end(), a);
+        auto right = upper_bound(points.begin(), points.end(), b);
+        
+        int count = right - left;
+        cout << count << endl;
     }
-
-
-    // treeset에서 정점을 작은 번호부터 뽑으면서
-    // 각 정점별로 1번부터 순서대로 매칭하여
-    // 그 결과를 hashmap에 넣어줍니다.
-
-
-
 
     return 0;
 }

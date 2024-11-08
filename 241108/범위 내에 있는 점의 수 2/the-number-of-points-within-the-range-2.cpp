42f14734b6b932429b93905f929ba0d453f4a7a6
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-08 18:02:08 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 700ms, Memory: 0MB, Status: Passed - CodeTree

--- 241108/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.cpp
+++ 241108/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 using namespace std;
 
 int main() {
@@ -13,15 +14,35 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         cin >> afds;
-        for (int i = afds; i < 1000001; i++) { arr[i]++; }
-        //v.push_back(arr[i]);
+        
+        v.push_back(afds);
     }
-    //sort(v.begin(), v.end());
+    sort(v.begin(), v.end());
+    map<int,int> map;
 
-    for (int i = 0; i < q; i++) {
-        int a, b;
+    int c = 1;
+    for(int i:v){
+        map[i]=c++;
+    }
+
+    for (int j = 0; j < q; j++) {
+        int a, b, i;
         cin >> a >> b;
-        cout << arr[b] - arr[a - 1] << endl;
+        int count1 = 0; 
+        for(i = a - 1; i>=0;i--){
+            if(map.find(i)!=map.end()){
+                count1 = map[i];
+                break;
+            }
+        }
+        int count2 = count1;
+        for(i = b; i>=a - 1;i--){
+            if(map.find(i)!=map.end()){
+                count2 = map[i];
+                break;
+            }
+        }
+        cout << count2 - count1 << endl;
     }
     // 여기에 코드를 작성해주세요.
     return 0;

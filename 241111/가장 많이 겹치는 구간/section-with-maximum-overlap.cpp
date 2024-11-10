4684c26bf1e24420d3e648db359999a4fe9d908e
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-11 01:05:04 +0900

    Title: 가장 많이 겹치는 구간, Time: 14ms, Memory: 1MB, Status: Passed - CodeTree

--- 241111/가장 많이 겹치는 구간/section-with-maximum-overlap.cpp
+++ 241111/가장 많이 겹치는 구간/section-with-maximum-overlap.cpp
@@ -22,37 +22,23 @@ int main() {
     // 각 선분을 두 지점으로 나눠서
     // +1, -1로 담은 뒤,
     // 정렬해줍니다.
-    vector<pair<int, int> > points;
+    //vector<pair<int, int> > points;
     cin >> n;
+    int points[200001] = { 0 };
     for (int i = 0; i < n; i++) {
         int x1, x2; cin >> x1 >> x2;
         //tie(x1, x2) = segments[i];
-        points.push_back(make_pair(x1, +1)); // 시작점
-        points.push_back(make_pair(x2, -1)); // 끝점
+        points[x1]++; points[x2]--;
     }
 
-    // 정렬을 진행합니다.
-    sort(points.begin(), points.end());
-
-    // x = k 전까지
-    // 각 위치에 적혀있는 숫자들의 합을 구해줍니다.
 
     int res = 0;
     int sum_val = 0;
-    for (int k = 1; k <= points[2*n-1].first; k++) {
-        for (int i = 0; i < 2 * n; i++) {
-            int x, v;
-            tie(x, v) = points[i];
+    for (int k = 1; k <= 200000; k++) {
+        sum_val += points[k];
 
-            // x가 k 이상이 되면 종료합니다.
-            if (x >= k)
-                break;
 
-            // 적혀있는 가중치를 전부 더해줍니다.
-            sum_val += v;
-        }
         res = max(sum_val, res);
-        sum_val = 0;
     }
     
 

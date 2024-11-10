81ffb9d7c2d27aaab302fce5f8d9743727706120
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-10 18:53:49 +0900

    Title: 구간의 합이 같게 나누기, Time: 1052ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

---
+++ 241110/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <climits>
+
+using namespace std;
+int arr[100001];
+long long L[100001] = {};
+long long R[100001] = {};
+int main() {
+    int n; cin >> n;
+    //int x, y; cin >> x >> y;
+    arr[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+
+
+    L[1] = arr[1];
+    for (int i = 2; i <= n; i++)
+        L[i] = L[i - 1] + arr[i];
+
+
+    // R 배열을 채워줍니다.
+    R[n] = arr[n];
+    for (int i = n - 1; i >= 1; i--)
+        R[i] = R[i + 1] + arr[i];
+
+    int m = 0;
+    for (int i = 2; i <= n - 2; i++) {
+        
+        if (L[i] == R[i + 1]) {
+            for (int j = 1; j < i; j++) {
+                for (int k = i + 1; k <= n - 1; k++) {
+                    if (L[j] == L[i] - L[j] && L[k] - L[i] == R[k + 1]) {
+                        m++;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << m;
+    return 0;
+}

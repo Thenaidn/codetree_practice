125b7b55785a6b1588b30a1cc4489b542f4a1efa
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-15 15:43:18 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241115/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int arr[100000] = {0,};
+int search(int target){
+    int idx = -2;
+    int count = 0;
+    // 이진탐색을 진행합니다.
+    int left = 1, right = n;
+    while (left <= right) {
+        count++;
+        int mid = (left + right) / 2;
+        if(mid == target) { // 찾았다면 해당 index를 반환합니다.
+            idx = mid;
+            break;
+        }
+        
+        if(mid > target) // 찾으려는 숫자가 더 작다면
+            right = mid - 1;  // 왼쪽 구간으로 이동해야 합니다.
+        else                  // 찾으려는 숫자가 더 크다면
+            left = mid + 1;   // 오른쪽 구간으로 이동해야 합니다.
+    }
+    
+    return count;
+}
+#include <climits>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n; int a, b; cin >> a >> b;
+    int mi = INT_MAX; int ma = INT_MIN;
+    for(int i=a;i<=b;i++){
+        int res = search(i);
+        mi = min(mi,res);
+        ma = max(ma,res);
+    }
+    cout << mi << " " << ma;
+    return 0;
+}

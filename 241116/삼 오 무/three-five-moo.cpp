89133b9118375b2b51a3170200201711e8484b8e
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-16 15:40:53 +0900

    Title: 삼 오 무, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241116/삼 오 무/three-five-moo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    long long n;
+
+    cin >> n;
+
+    long long left = 1;                          // 가장 작은 숫자 값을 설정합니다.
+    long long right = 1000000000;                        // 가장 큰 숫자 값을 설정합니다.
+    long long min_num = 0;                      // 최소이므로, 답이 될 수 있는 값보다 더 큰 값으로 설정합니다.
+    while (left <= right) {                // [left, right]가 유효한 구간이면 계속 수행합니다.
+        long long mid = (left + right) / 2;      // 가운데 위치를 선택합니다.
+        long long tmp = mid - (mid / 3) - (mid / 5) + (mid / 15);
+        if(tmp == n){cout << mid; return 0;}
+        if(tmp > n){right = mid - 1;}
+        else{left = mid+1;}
+    }
+
+    //cout << min_num;                       // 조건을 만족하는 최소 n 값을 출력합니다.
+    return 0;
+}

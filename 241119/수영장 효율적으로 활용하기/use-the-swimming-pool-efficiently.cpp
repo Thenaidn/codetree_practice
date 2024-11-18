1d442ca21c00d9c1e0ca4485238da470343fede1
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-19 01:56:29 +0900

    Title: 수영장 효율적으로 활용하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241119/수영장 효율적으로 활용하기/use-the-swimming-pool-efficiently.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+
+bool isPossible(const vector<int>& times, int m, int maxLoad) {
+    int lanes = 1;
+    int currentLoad = 0;
+
+    for (int time : times) {
+        if (currentLoad + time <= maxLoad) {
+            currentLoad += time;
+        }
+        else {
+            lanes++;
+            currentLoad = time;
+            if (lanes > m) return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> times(n);
+    for (int i = 0; i < n; i++) {
+        cin >> times[i];
+    }
+
+    int left = 1;
+    int right = 1440;
+    int result = right;
+
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (isPossible(times, m, mid)) {
+            result = mid;
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+
+    cout << result;
+    return 0;
+}

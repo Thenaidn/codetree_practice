5712f84ac4589bb1e7bc954e367e66da675adebe
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-19 17:48:00 +0900

    Title: 자연수 M/2개의 쌍, Time: 143ms, Memory: 3MB, Status: Passed - CodeTree

--- 241119/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
+++ 241119/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
@@ -1,39 +1,51 @@
 #include <iostream>
-#include <set>
+#include <queue>
 #include <vector>
-#include <climits>
-#include <algorithm>
 using namespace std;
 
 int main() {
-    set<int> s;
-    int n; cin >> n;
-    vector<pair<long long, long long>> v(n);
+    int n;
+    cin >> n;
 
-    for (int i = 0; i < n; i++) { long long a, b; cin >> a >> b; v[i] = { b,a }; }
+    priority_queue<pair<long long, long long>> maxHeap; // (값, 개수)
+    priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<>> minHeap;
 
-    sort(v.begin(), v.end());
-    long long res = 0;
+    for (int i = 0; i < n; i++) {
+        long long x, y;
+        cin >> x >> y;
 
-    while (v.size() > 0) {
-        long long a, b, c, d;
-        a = v[0].first; b = v[0].second;
-        c = v[v.size() - 1].first; d = v[v.size() - 1].second;
-        res = max(res, a + c);
-        long long tmp = min(b, d);
-        v[0].second -= tmp;
-        if (v[0].second == 0) {
-            v.erase(v.begin());
-        }
-        if (v.size() >= 1) {
-            v[v.size() - 1].second -= tmp;
-            if (v[v.size() - 1].second == 0) {
-                v.erase(v.begin() + v.size() - 1);
-            }
-        }
-        
+        maxHeap.push({y, x});
+        minHeap.push({y, x});
     }
 
-    cout << res;
+    long long res = 0;
+
+    while (!maxHeap.empty() && !minHeap.empty()) {
+        auto maxTop = maxHeap.top();
+        maxHeap.pop();
+
+        auto minTop = minHeap.top();
+        minHeap.pop();
+
+        long long maxVal = maxTop.first;
+        long long maxCount = maxTop.second;
+
+        long long minVal = minTop.first;
+        long long minCount = minTop.second;
+
+        res = max(res, maxVal + minVal);
+
+        long long pairCount = min(maxCount, minCount);
+
+
+        if (maxCount > pairCount) {
+            maxHeap.push({maxVal, maxCount - pairCount});
+        }
+        if (minCount > pairCount) {
+            minHeap.push({minVal, minCount - pairCount});
+        }
+    }
+
+    cout << res << endl;
     return 0;
 }

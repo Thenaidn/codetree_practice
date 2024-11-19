063982d27ab3b3f783b43af1e9a6d2a89d2a9237
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-19 15:55:41 +0900

    Title: 수 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241119/수 채우기/fill-in-number.cpp
+++ 241119/수 채우기/fill-in-number.cpp
@@ -4,13 +4,16 @@ using namespace std;
 int main() {
     int n; cin >> n;
     int res=0;
+    int tmp = n;
     res+=n/5;
     n%=5;
     res+=n/2;
     n%=2;
     if(n == 1){
-        res+=2;
+        if(tmp > 5){res+=2;}
+        else{res=-1;}
     }
+
     cout<<res;
     return 0;
 }

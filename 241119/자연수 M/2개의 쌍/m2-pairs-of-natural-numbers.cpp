d08ee3164aea4e828d3bdb06efb1893b09914fb6
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-19 17:42:25 +0900

    Title: 자연수 M/2개의 쌍, Time: 1085ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 241119/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
+++ 241119/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
@@ -8,27 +8,30 @@ using namespace std;
 int main() {
     set<int> s;
     int n; cin >> n;
-    vector<pair<int, int>> v(n);
+    vector<pair<long long, long long>> v(n);
 
-    for (int i = 0; i < n; i++) { int a, b; cin >> a >> b; v[i] = { b,a }; }
+    for (int i = 0; i < n; i++) { long long a, b; cin >> a >> b; v[i] = { b,a }; }
 
     sort(v.begin(), v.end());
-    int res = INT_MAX;
+    long long res = 0;
 
     while (v.size() > 0) {
-        int a, b, c, d;
+        long long a, b, c, d;
         a = v[0].first; b = v[0].second;
         c = v[v.size() - 1].first; d = v[v.size() - 1].second;
-        res = min(res, a + c);
-        b--;
-        if (b == 0) {
+        res = max(res, a + c);
+        long long tmp = min(b, d);
+        v[0].second -= tmp;
+        if (v[0].second == 0) {
             v.erase(v.begin());
         }
-        if (a == c) { d = b; }
-        d--;
-        if (d == 0) {
-            v.erase(v.begin() + v.size() - 1);
+        if (v.size() >= 1) {
+            v[v.size() - 1].second -= tmp;
+            if (v[v.size() - 1].second == 0) {
+                v.erase(v.begin() + v.size() - 1);
+            }
         }
+        
     }
 
     cout << res;

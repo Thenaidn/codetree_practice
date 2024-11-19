fcf719ad3b522565dcaa5f40e7d66dc34ba32221
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-19 15:44:25 +0900

    Title: 회의실 준비 구현, Time: 46ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 241119/회의실 준비 구현/implement-scheduling-meeting-room.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<int,int> a, pair<int,int> b){
+    if(a.second == b.second){return a.second - a.first < b.second - b.first;}
+    else{return a.second < b.second;}
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n; cin >> n;
+    vector<pair<int,int>> v(n);
+    for(int i=0;i<n;i++){
+        int s, e; cin >> s >> e;
+        v[i] = {s,e};
+    }
+    sort(v.begin(), v.end(), compare);
+
+    int res = 1;
+    int end = v[0].second;
+
+    for(int i=1;i<n;i++){
+        if(v[i].first<end){continue;}
+        end = v[i].second; res++;
+    }
+
+    cout << res;
+    return 0;
+}

1c22b8b93a537e575212a9099241a2dbb310a7e5
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-21 14:37:19 +0900

    Title: G & H 반전시키기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241121/G & H 반전시키기 3/reversing-g-and-h-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    string t1, t2; int n;
+    cin >> n >> t1 >> t2;
+    vector<int> c(n);
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        if (t1[i] != t2[i]) { c[i] = 0; }
+        else { c[i] = 1; }
+    }
+    int prev = c[0];
+    int cnt = 0;
+    if (prev == 0) { res++; }
+
+    for (int i = 1; i < n; i++) {
+        if (c[i] == 0) {
+            if (c[i] != prev) {
+                res++;
+                cnt = 0;
+            }
+            else {
+                cnt++;
+                if (cnt >= 4) { res++; cnt = 0; }
+            }
+        }
+
+        prev = c[i];
+    }
+    cout << res;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

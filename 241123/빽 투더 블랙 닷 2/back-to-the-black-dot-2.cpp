59c0e11fc2b965f02daa24b9a2ccf711ec919f42
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-23 18:44:31 +0900

    Title: 빽 투더 블랙 닷 2, Time: 91ms, Memory: 5MB, Status: Passed - CodeTree

--- 241123/빽 투더 블랙 닷 2/back-to-the-black-dot-2.cpp
+++ 241123/빽 투더 블랙 닷 2/back-to-the-black-dot-2.cpp
@@ -7,12 +7,12 @@
 #define INF (int)1e9
 
 using namespace std;
-int dist_between_red;
+long long dist_between_red;
 // 변수 선언
 vector<pair<int, int>> graph[100001];
-vector<int> dist_from_a, dist_from_b, dist_from_c;
+vector<long long> dist_from_a, dist_from_b, dist_from_c;
 
-void dijkstra(int start, vector<int>& dist, int n) {
+void dijkstra(int start, vector<long long>& dist, int n) {
     dist.assign(n + 1, INF);
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
 
@@ -58,16 +58,17 @@ int main() {
     // 다익스트라 실행
     dijkstra(a, dist_from_a, n);
     dist_between_red = dist_from_a[b];
+    dijkstra(b, dist_from_b, n);
 
     // 각 정점에서 a, b, c 중 가장 가까운 거리의 최대값 계산
-    int max_closest_distance = INF;
+    long long max_closest_distance = INF;
     for (int i = 1; i <= n; i++) {
         if(i==a||i==b){continue;}
 
-        dijkstra(i, dist_from_b, n);
+        
 
-        int closest_distance = min((dist_from_b[a] + dist_from_b[b])*2, 
-        dist_from_b[a] + dist_from_b[b] + dist_between_red);
+        long long closest_distance = min((dist_from_a[i] + dist_from_b[i])*2, 
+        dist_from_a[i] + dist_from_b[i] + dist_between_red);
 
         if (closest_distance < INF) {
             max_closest_distance = min(max_closest_distance, closest_distance);

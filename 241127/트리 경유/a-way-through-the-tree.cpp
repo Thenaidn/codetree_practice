4e243d5ff113428febdfa0f162936525cb679407
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-11-27 23:00:18 +0900

    Title: 트리 경유, Time: 817ms, Memory: 4MB, Status: Passed - CodeTree

--- 241127/트리 경유/a-way-through-the-tree.cpp
+++ 241127/트리 경유/a-way-through-the-tree.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include <set>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
 set<int> painted;
-int find(int dest){
-    while(dest > 1){
+int find(int dst) {
+    int dest = dst;
+    int minone = INT_MAX;
+    while (dest > 1) {
+        
+        if (painted.find(dest) != painted.end()) { minone = min(minone, dest); }
         dest /= 2;
-        if(painted.find(dest) != painted.end()){ return dest ;}
     }
+    if (minone != INT_MAX) { return minone; }
+    painted.insert(dst);
     return 0;
 }
 
 int main() {
-   
+
     int n, q; cin >> n >> q;
-    for(int i=0;i<q;i++){
+    for (int i = 0; i < q; i++) {
         int a; cin >> a;
         int res = find(a);
-        if(res == 0){painted.insert(a);}
+
         cout << res << endl;
     }
     return 0;

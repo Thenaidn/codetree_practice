d6d200b3a014d2ff677138767d1df70ec816e76c
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-12-01 13:14:14 +0900

    Title: 노드의 인접한 공통 조상, Time: 17ms, Memory: 2MB, Status: Passed - CodeTree

--- 241201/노드의 인접한 공통 조상/adjacent-common-ancestor-nodes.cpp
+++ 241201/노드의 인접한 공통 조상/adjacent-common-ancestor-nodes.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <set>
 using namespace std;
 
 const int MAXN = 10001; // 최대 노드 수
@@ -8,6 +8,7 @@ vector<int> tree[MAXN];
 int parent[MAXN];      // 부모 정보
 int depth[MAXN];       // 깊이 정보
 bool visited[MAXN];    // 방문 여부
+set<int> s;
 
 // DFS로 부모와 깊이 정보 설정
 void dfs(int node, int d) {
@@ -25,12 +26,12 @@ void dfs(int node, int d) {
 // LCA 계산
 int findLCA(int u, int v) {
     // u와 v의 깊이를 동일하게 맞추기
-    while(depth[u] != depth[v]) {
-    if(depth[u] > depth[v])
-        u = parent[u];
-    else
-        v = parent[v];
-}
+    while (depth[u] != depth[v]) {
+        if (depth[u] > depth[v])
+            u = parent[u];
+        else
+            v = parent[v];
+    }
 
     // 공통 조상을 찾을 때까지 부모로 이동
     while (u != v) {
@@ -50,11 +51,18 @@ int main() {
         int a, b;
         cin >> a >> b;
         tree[a].push_back(b);
+        s.insert(b);
         tree[b].push_back(a);
     }
 
-    // 루트 노드에서 시작하여 DFS 수행 (1번 노드를 루트로 가정)
-    dfs(1, 0);
+    // 루트 탐색: 부모가 없는 노드를 루트로 가정
+    fill(parent, parent + MAXN, -1);
+    for (int i = 1; i <= n; i++) {
+        if (s.find(i) == s.end()) {
+            dfs(i, 0); // 루트에서 DFS 시작
+            break;
+        }
+    }
 
     // 공통 조상을 구할 두 노드 입력
     int u, v;

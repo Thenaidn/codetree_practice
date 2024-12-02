cf395b4b12a65250206e205777c322e757a2b163
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-12-02 15:23:54 +0900

    Title: 집합의 원소, Time: 96ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 241202/집합의 원소/elements-of-a-set.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+#define MAXN 100001
+
+int uf[MAXN];
+
+int Find(int x){
+    if(uf[x] == x){return x;}
+    int root_node = Find(uf[x]);
+    uf[x] = root_node;
+    return root_node;
+}
+
+void Union(int x, int y){
+    int x1 = Find(x); int y1 = Find(y);
+    uf[x1] = y1;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    
+    int n, m; cin >> n >> m;
+    for(int i=1;i<=n;i++){uf[i] = i;}
+    while(m--){
+        int q, a, b;
+        cin >> q >> a >> b;
+        if(q == 0){
+            Union(a,b);
+        }
+        else{
+            if(uf[a] == uf[b]){cout << 1 << endl;}
+            else{cout << 0 << endl;}
+        }
+    }
+    return 0;
+}

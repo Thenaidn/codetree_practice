bd4aa5f7a60ad60204cec47d9cb76b7309f88e75
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-12-02 15:46:02 +0900

    Title: 연결된 정점, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241202/연결된 정점/connected-vertex.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+#define MAXN 5
+
+int uf[MAXN];
+unordered_set<int> root[MAXN];
+
+int Find(int x) {
+    if (uf[x] == x) { return x; }
+    int root_node = Find(uf[x]);
+    root[x].erase(x);
+    root[root_node].insert(x);
+    uf[x] = root_node;
+    return root_node;
+}
+
+void Union(int x, int y) {
+    int x1 = Find(x); int y1 = Find(y);
+    root[x1].erase(x1);
+    root[y1].insert(x1);
+    uf[x1] = y1;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n, m; cin >> n >> m;
+    for (int i = 1; i <= n; i++) { uf[i] = i; root[i].insert(i); }
+
+    while (m--) {
+        char q; int a, b;
+        cin >> q;
+        if (q == 'x') {
+            cin >> a >> b;
+            if (uf[a] != uf[b]) {
+                Union(a, b);
+            }
+        }
+        else {
+            cin >> a;
+            cout << root[uf[a]].size() << endl;
+        }
+    }
+    return 0;
+}

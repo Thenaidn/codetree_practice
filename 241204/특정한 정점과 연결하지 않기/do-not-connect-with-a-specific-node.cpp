3a4daf43b8811ae2b4a258c323cccca1d0a4b40b
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-12-04 19:07:18 +0900

    Title: 특정한 정점과 연결하지 않기, Time: 34ms, Memory: 2MB, Status: Wrong Answer - CodeTree

---
+++ 241204/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define MAXN 100005
+
+int uf[MAXN], Size[MAXN];
+
+bool cmp(pair<int, int> a, pair<int, int> b) {
+    if (a.first == b.first) {
+        return a.second > b.second;
+    }
+    return a.first > b.first;
+}
+
+int Find(int x) {
+    if (x == uf[x]) return x;
+    return uf[x] = Find(uf[x]);
+}
+
+void Union(int x, int y) {
+    int rootX = Find(x);
+    int rootY = Find(y);
+    if (rootX != rootY) {
+        if (rootX > rootY) {
+            swap(rootX, rootY);
+        }
+        uf[rootY] = rootX;
+        Size[rootX] += Size[rootY];
+        Size[rootY] = 0;
+    }
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //int res = 0;
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        uf[i] = i;
+        Size[i] = 1;
+    }
+    //m = n - 2;
+
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+        Union(a, b);
+    }
+
+    int a, b, k; cin >> a >> b >> k;
+
+    int res = Size[Find(a)];
+
+    vector<pair<int, int>> sizes;
+    //sizes.push_back({ -1, -1 });
+    for (int i = 1; i <= n; i++) {
+        sizes.push_back({ Size[i], i });
+    }
+    sort(sizes.begin(), sizes.end(), cmp);
+
+
+    for (int i = 0; i < sizes.size(); i++) {
+        if (sizes[i].second == a) { continue; }
+        if (Find(b) == Find(sizes[i].second)) { continue; }
+        res += sizes[i].first;
+        k--;
+        if (k == 0) { break; }
+    }
+   
+    cout << res;
+    return 0;
+}

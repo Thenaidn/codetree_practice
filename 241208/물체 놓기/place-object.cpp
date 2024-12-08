5a0364546aa32ba05276f0815ddce6055ab0def3
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-12-08 16:05:56 +0900

    Title: 물체 놓기, Time: 32ms, Memory: 1MB, Status: Passed - CodeTree

--- 241208/물체 놓기/place-object.cpp
+++ 241208/물체 놓기/place-object.cpp
@@ -1,104 +1,68 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
-#include <queue>
+#include <algorithm>
+
+#define MAX_N 300
 
 using namespace std;
 
 // 변수 선언
-#define MAXN 100001
-int n = 100005, m = 7;
-vector<pair<int, int> > graph[MAXN];
-priority_queue<pair<int, int> > pq;
+int n, m;
+int graph[MAX_N + 1][MAX_N + 1];
+bool visited[MAX_N + 1];
 
-int dist[MAXN];
-bool visited[MAXN];
-int weight[MAXN];
+int dist[MAX_N + 1];
 
 int main() {
-    // 주어진 간선 정보 (x, y, z)
-    // x <-> y로 향하는 간선이 있으며, 가중치는 z 
-    cin >> n; m = n;
+    // 입력
+    cin >> n;
 
-
-    int mi = (int)1e9;
-    int start = 0;
-    for(int i=1;i<=n;i++){
-        cin >> weight[i];
-        if(mi > weight[i]){
-            start = i; mi = weight[i];
-        }
-    }
-
-    // 그래프를 인접리스트로 표현합니다.
-    for(int x = 1; x <= m; x++) {
-        for(int y = x + 1; y<=m;y++){
-            int z; cin >> z; if(z==0){continue;}
-            graph[x].push_back(make_pair(y, z));
-            graph[y].push_back(make_pair(x, z));
-        }
-        
-    }
+    // 그래프를 인접행렬로 표현
+    // 더미 노드(0번 노드)를 만들어서 1번부터 n번까지 노드를 (직접 물체를 놓는 비용) 으로 연결합니다.
+    for(int i = 1; i <= n; i++)
+        cin >> graph[0][i];
+    
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            cin >> graph[i][j];
     
     // 그래프에 있는 모든 노드들에 대해
     // 초기값을 전부 아주 큰 값으로 설정
     // INT_MAX 그 자체로 설정하면
     // 후에 덧셈 진행시 overflow가 발생할 수도 있으므로
     // 적당히 큰 숫자로 적어줘야함에 유의!
-    for(int i = 1; i <= n; i++)
+    for(int i = 0; i <= n; i++)
         dist[i] = (int)1e9;
 
-    // 시작위치에는 dist값을 0으로 설정
-    dist[start] = 0;
+    // 시작위치(더미노드)에는 dist값을 0으로 설정
+    dist[0] = 0;
 
-    // 우선순위 큐에 시작점을 넣어줍니다.
-    // 거리가 가까운 곳이 먼저 나와야 하며
-    // 해당 지점이 어디인지에 대한 정보도 필요하므로
-    // (거리, 정점 번호) 형태로 넣어줘야 합니다.
-    // 단, c++에서는 우선순위 큐가 큰 값을 기본으로 뽑아주기 때문에
-    // -를 붙여서 넣어줘야 함에 유의합니다.
-    pq.push(make_pair(-0, start));
+    // O(|V|^2) 프림 코드
+    int ans = 0;
+    for(int i = 0; i <= n; i++) {
+        // V개의 정점 중 
+        // 아직 방문하지 않은 정점 중
+        // dist값이 가장 작은 정점을 찾아줍니다.
+        int min_index = -1;
+        for(int j = 0; j <= n; j++) {
+            if(visited[j])
+                continue;
+            
+            if(min_index == -1 || dist[min_index] > dist[j])
+                min_index = j;
+        }
 
-    // O(|E|log|V|) 프림 코드
-    // 우선순위 큐에
-    // 원소가 남아있다면 계속 진행해줍니다.
-    int ans = weight[start];
-    while(!pq.empty()) {
-        // 가장 거리가 가까운 정보를 받아온 뒤, 원소를 제거해줍니다.
-        int min_dist, min_index;
-        tie(min_dist, min_index) = pq.top();
-        pq.pop();
-
-        // -를 다시 붙여줍니다.
-        min_dist = -min_dist;
-
-        // 우선순위 큐를 이용하면
-        // 같은 정점의 원소가 
-        // 여러 번 들어가는 문제가 발생할 수 있어
-        // 이미 계산해본 적이 있는 경우라면
-        // 바로 패스해줍니다.
-        if(visited[min_index])
-            continue;
-
-        // visited 값을 true로 바꿔주고
-        // 답을 갱신해줍니다. 
+        // 최솟값에 해당하는 정점에 방문 표시를 진행합니다.
         visited[min_index] = true;
 
-        ans += min(min_dist, weight[min_index]);
+        // mst 값을 갱신해줍니다.
+        ans += dist[min_index];
 
         // 최솟값에 해당하는 정점에 연결된 간선들을 보며
-        // 최솟값을 갱신해줍니다.
-        for(int j = 0; j < (int) graph[min_index].size(); j++) {
-            int target_index, target_dist;
-            tie(target_index, target_dist) = graph[min_index][j];
-            
-            // 현재 위치에서 연결된 간선으로 가는 것이 더 작다면
-            int new_dist = target_dist;
-            if(dist[target_index] > new_dist) {
-                // 값을 갱신해주고, 우선순위 큐에 해당 정보를 넣어줍니다.
-                dist[target_index] = new_dist;
-                pq.push(make_pair(-new_dist, target_index));
-            }
+        // 시작점으로부터의 최솟값을 갱신해줍니다.
+        for(int j = 1; j <= n; j++) {
+            dist[j] = min(dist[j], graph[min_index][j]);
         }
     }
 

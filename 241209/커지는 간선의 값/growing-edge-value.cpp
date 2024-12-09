b85f4fb5672d9976444173db56bb6143efb9f89f
1971261-kjs <149080127+Thenaidn@users.noreply.github.com>
2024-12-09 16:15:18 +0900

    Title: 커지는 간선의 값, Time: 197ms, Memory: 7MB, Status: Passed - CodeTree

--- 241209/커지는 간선의 값/growing-edge-value.cpp
+++ 241209/커지는 간선의 값/growing-edge-value.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
-#include <tuple>
-#include <algorithm>
 #include <vector>
+#include <tuple>
+#include <queue>
 
 using namespace std;
-const int MAXN = 100001;
-// 변수 선언
-// 정점의 수 : 5, 간선의 수 : 7인 그래프
-int n = 5, m = 7;
-int graph[MAXN][MAXN];
-bool visited[MAXN];
 
-int dist[MAXN];
+// 변수 선언
+int n = 5, m = 7;
+vector<pair<int, int> > graph[100001];
+priority_queue<pair<int, int> > pq;
+
+int dist[100001];
+bool visited[100001];
 
 int main() {
-    int k;
-    cin >> n >> m >> k;
-    // 그래프를 인접행렬로 표현
+    int k; cin >> n >> m >> k;
+
+    // 그래프를 인접리스트로 표현합니다.
     for(int i = 1; i <= m; i++) {
         int x, y, z;
         cin >> x >> y >> z;
-        graph[x][y] = (graph[x][y] == 0) ? z : min(graph[x][y], z);
-        graph[y][x] = (graph[y][x] == 0) ? z : min(graph[y][x], z);
+        graph[x].push_back(make_pair(y, z));
+        graph[y].push_back(make_pair(x, z));
     }
     
     // 그래프에 있는 모든 노드들에 대해
@@ -35,43 +35,58 @@ int main() {
     // 시작위치에는 dist값을 0으로 설정
     dist[1] = 0;
 
-    vector<int> v;
-    // O(|V|^2) 프림 코드
+    // 우선순위 큐에 시작점을 넣어줍니다.
+    // 거리가 가까운 곳이 먼저 나와야 하며
+    // 해당 지점이 어디인지에 대한 정보도 필요하므로
+    // (거리, 정점 번호) 형태로 넣어줘야 합니다.
+    // 단, c++에서는 우선순위 큐가 큰 값을 기본으로 뽑아주기 때문에
+    // -를 붙여서 넣어줘야 함에 유의합니다.
+    pq.push(make_pair(-0, 1));
+
+    // O(|E|log|V|) 프림 코드
+    // 우선순위 큐에
+    // 원소가 남아있다면 계속 진행해줍니다.
     long long ans = 0;
-    for(int i = 1; i <= n; i++) {
-        // V개의 정점 중 
-        // 아직 방문하지 않은 정점 중
-        // dist값이 가장 작은 정점을 찾아줍니다.
-        int min_index = -1;
-        for(int j = 1; j <= n; j++) {
-            if(visited[j])
-                continue;
-            
-            if(min_index == -1 || dist[min_index] > dist[j])
-                min_index = j;
-        }
+    while(!pq.empty()) {
+        // 가장 거리가 가까운 정보를 받아온 뒤, 원소를 제거해줍니다.
+        int min_dist, min_index;
+        tie(min_dist, min_index) = pq.top();
+        pq.pop();
 
-        // 최솟값에 해당하는 정점에 방문 표시를 진행합니다.
+        // -를 다시 붙여줍니다.
+        min_dist = -min_dist;
+
+        // 우선순위 큐를 이용하면
+        // 같은 정점의 원소가 
+        // 여러 번 들어가는 문제가 발생할 수 있어
+        // 이미 계산해본 적이 있는 경우라면
+        // 바로 패스해줍니다.
+        if(visited[min_index])
+            continue;
+
+        // visited 값을 true로 바꿔주고
+        // 답을 갱신해줍니다. 
         visited[min_index] = true;
-
-        // mst 값을 갱신해줍니다.
-        ans += dist[min_index];
-        v.push_back(dist[min_index]);
+        ans += min_dist;
 
         // 최솟값에 해당하는 정점에 연결된 간선들을 보며
-        // 시작점으로부터의 최솟값을 갱신해줍니다.
-        for(int j = 1; j <= n; j++) {
-            // 간선이 존재하지 않는 경우에는 넘어갑니다.
-            if(graph[min_index][j] == 0)
-                continue;
-
-            dist[j] = min(dist[j], graph[min_index][j]);
+        // 최솟값을 갱신해줍니다.
+        for(int j = 0; j < (int) graph[min_index].size(); j++) {
+            int target_index, target_dist;
+            tie(target_index, target_dist) = graph[min_index][j];
+            
+            // 현재 위치에서 연결된 간선으로 가는 것이 더 작다면
+            int new_dist = target_dist;
+            if(dist[target_index] > new_dist) {
+                // 값을 갱신해주고, 우선순위 큐에 해당 정보를 넣어줍니다.
+                dist[target_index] = new_dist;
+                pq.push(make_pair(-new_dist, target_index));
+            }
         }
     }
-    //cout << ans;
-    //ans = 0;
+
     
-    for(int i=0;i<v.size() - 1;i++){
+    for(int i=1;i<n - 1;i++){
         ans += i * k;
     }
 
